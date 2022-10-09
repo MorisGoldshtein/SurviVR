75cbb6108a6e5df2cafcc0dc3611439f777a0c34
kenko1290 <ken.ko1290@gmail.com>
2022-10-08 23:39:46 -0400

    Added ability to cycle through scenes, rotate player with Q/E or J/K, and dungeon scene

--- Assets/Scripts/KeyboardMovement.cs
+++ Assets/Scripts/KeyboardMovement.cs
@@ -9,15 +9,25 @@ public class KeyboardMovement : MonoBehaviour{
         float horizontalInput = Input.GetAxis("Horizontal");
         //Value changes when W/S/UpArrow/DownArrow pressed -> between -1 and 1
         float verticalInput = Input.GetAxis("Vertical");
-
+        // Player can rotate to left or right using keyboard keys 'Q' or 'E'
+        bool rotateLeft = Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.J);
+        bool rotateRight = Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.K);
+        // Passed to transform.Rotate() to rotate the player
+        float rotation = 0;
         // Combine the inputs together into a vector
         Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
         
         //Makes diagonal movement not go faster than going vertical/horizontal only
-        movementDirection.Normalize(); 
+        movementDirection.Normalize();
+
+        if (rotateLeft)
+            rotation = -100;
+        if(rotateRight)
+            rotation = 100;
 
         // Apply the vector for movement
         transform.Translate(movementDirection * 5 * Time.deltaTime);
+        transform.Rotate(0f, rotation*Time.deltaTime,0f);
 
 
         /*
--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -3,14 +3,22 @@ using System.Collections;
 //using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour{
+    string[] scenes = {"Test","Test2","Fist","Dungeon"};
+
     void Update(){
+        int current_scene = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
         if (Input.GetKeyDown(KeyCode.O)){
             Debug.Log("Pressed O");
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Test");
+            current_scene++;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scenes[current_scene % scenes.Length]);
         }
         if (Input.GetKeyDown(KeyCode.L)){
             Debug.Log("Pressed L");
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Test2");
+            if(current_scene == 0)
+                current_scene = scenes.Length;
+            current_scene--;
+            UnityEngine.SceneManagement.SceneManager.LoadScene(scenes[current_scene % scenes.Length]);
         }
+        
     }
 }

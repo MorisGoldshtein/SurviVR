5c06e63371c5e3d7658470e70de3630465c4ef4a
Moris Goldshtein <Moris.Goldshtein22@myhunter.cuny.edu>
2022-10-31 22:46:50 -0400

    Merge Moris, Ken, Lucas

---
+++ Assets/WildWestAssets/Main_Menu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("quitted");
+    }
+
+    public void PlayLucas()
+    {
+        SceneManager.LoadScene("WildWest");
+    }
+
+    public void PlayDillon()
+    {
+        SceneManager.LoadScene("Fist");
+    }
+
+    public void PlayKen()
+    {
+        SceneManager.LoadScene("Dmg Kill");
+    }
+
+    public void PlayMoris()
+    {
+        SceneManager.LoadScene("Obj Grab");
+    }
+}
